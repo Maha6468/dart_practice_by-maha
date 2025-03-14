194df8bb890d32bd4937d21799992c0552e84d6c
Maha6468 <motherma7633@gmail.com>
2025-03-14 20:52:44 +0600

    2nd practice of list

--- bin/lib/List.dart
+++ bin/lib/List.dart
@@ -1,25 +1,10 @@
 void main()
 {
-  List<int> numbers=[10,20,30,40,50];
-  List<String> fruits=['apple','mango'];
+  List<int> numbers=[1,2,];
+  numbers.addAll([4,5,6]);
   print(numbers);
-  print(numbers.length);
-  print(fruits);
-  fruits.add('banana');
-  print(fruits);
-  numbers.remove(20);
+  numbers.insert(2,3);
   print(numbers);
-  print(fruits[1]);
-  String pot=fruits[2];
-  print(pot);
-  numbers[1]=20;
-  print(numbers);
-  print(fruits);
-  print(fruits[1]="orange");
-  print(fruits);
-  print(fruits.length);
-  print("list size:${fruits.length}");
-
 
 
 
