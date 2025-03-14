438ff8522c77f53e9d83edca32a1b9762787376a
Maha6468 <motherma7633@gmail.com>
2025-03-15 00:59:04 +0600

    assignment operator

--- bin/lib/operator.dart
+++ bin/lib/operator.dart
@@ -3,11 +3,21 @@ void main()
   int a,b;
   a=20;
   b=10;
+  //arithmatic operator
   print('a+b: ${a+b}');
   print('a-b: ${a-b}');
   print('a*b: ${a*b}');
   print('a/b: ${a/b}');
   print('a%b: ${a%b}');
+// assignment operator
+  print(a);
+  a+=a;
+  print(a);
+  a-=a;
+  print(a);
+  b*=b;
+  print(b);
+
 
 
 
